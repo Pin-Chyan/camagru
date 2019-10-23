ed843d0e08b5d3536403fab4d3b8ef35e283196d
Liam Krieling <lkrielin@c3r6s7.wethinkcode.co.za>
2019-10-23 15:28:12 +0200

    install fix

--- database/imagetable.php
+++ database/imagetable.php
@@ -1,43 +1,11 @@
 <?php
-function conn(){
-    $user = "senpai";
-    $pass = "noticeme";
-    $host = "localhost:3306";
-    $name = "senpai";
-    try{
-        $conn = new PDO("mysql:host=$host",$user,$pass);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "\nconnected";
-    }
-    catch(PDOException $e){
-        echo "\nok fuck";//echo "\n".$e->getMessage();
-    }
-    $conn->exec("DROP DATABASE $name");
-    $conn->exec("CREATE DATABASE $name");
-    try{
-        $senapi = new PDO("mysql:host=$host;dbname=senpai",$user,$pass);
-        $senpai->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "\nconnected";
-    }
-    catch(PDOException $e){
-        echo "\nok fuck";//echo "\n".$e->getMessage();
-    }
-    return ($senpai);
-}
-$senpai = conn();
-$id = "id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY";
-$author = "author VARCHAR(10)";
-$date = "upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP";
-$image = "img64 TEXT";
-$img_ex = "extention TEXT(10)";
-$image_like = "likes INT(10)";
-$senpai->exec("CREATE TABLE images ($id,$author,$date,$image)");
+require("init.php");
 
-function uploadimg($senpai,$author,$imglocale){
-    $binary_senpai = base64_encode(file_get_contents($imglocale));
-    $senpai->exec("INSERT INTO users (author,img64) VALUES ('$bianry_senpai','$author')");
+function uploadimg($author,$imglocale){
+    $senpai = Call_onee_san();
+    $binary_senpai = base64_encode(file_get_contents("database/senpai.jpeg"));
+    $senpai->exec("INSERT INTO images (img64,author) VALUES ('$bianry_senpai','$author')");
 }
 
-$mai = "mai-san";
-uploadimg($senpai,$mai,"senpai.jpeg");
+uploadimg("mai","senpai.jpeg");
 ?>
