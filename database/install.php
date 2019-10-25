ff571f586f2dcf0b4224f9e822fdc243b2edd7c5
Shane OLIVARI <solivari@c3r6s8.wethinkcode.co.za>
2019-10-25 09:11:27 +0200

    Working on like functions

--- database/install.php
+++ database/install.php
@@ -4,6 +4,7 @@ $pass = "noticeme";
 $host = "localhost:3306";
 $name = "senpai";
 include "database_info.php";
+require("init.php");
 
 
 //CREATE DATABASE
@@ -13,14 +14,14 @@ try{
 	$sql = "CREATE DATABASE `".$name."`";
 	$conn->exec("DROP DATABASE $name");
 	$conn->exec($sql);
-	echo "Senpai is born!\n";
+	echo "Senpai noticed me!\n";
 }
 catch(PDOException $e){
 	echo "senpai didn't notice us ;(\n".$e->getMessage();
 	die ();
 }
 
-///created user table
+///CREATE USER TABLE
 try {
 	$senpai = Call_onee_san();
 	$sql = "CREATE TABLE `users` (
@@ -37,10 +38,11 @@ try {
 	echo "Table users created\n";
 }
 catch (PDOException $e) {
-	echo "User table died miserbly: ".$e->getMessage()."\n";
+	echo "Users table died miserbly: ".$e->getMessage()."\n";
 }
 
 /*	Liams one incase he wants it.
+
 	"id INT(11) AUTO_INCREMENT PRIMARY KEY";
 	$tuser = "username VARCHAR(50)";
 	$temail = "email VARCHAR(50)";
@@ -51,7 +53,25 @@ catch (PDOException $e) {
 	$treg_date = "reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP";
 */
 
-////image table (lets call this the gallery guys!)
+////GALLERY TABLE (lets call this the gallery guys!)
+try {
+	$senpai = Call_onee_san();
+	$sql = "CREATE TABLE `gallery` (
+		`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		`userid` INT(11) NOT NULL,
+		`img` VARCHAR(100) NOT NULL,
+		`up_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+		FOREIGN KEY (userid) REFERENCES users(id)
+	  )";
+	  $senpai->exec($sql);
+	  echo "Gallery of onee-senpai exists!\n";
+}
+catch (PDOException $e) {
+	echo "Never speak to me or my family again ~ Gallery table".$e->getMessage()."\n";
+}
+
+/* Need to speak to Liam about all these columns (shane)
+
 $id = "id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY";
 $userid = "userid VARCHAR(50)";
 $date = "upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP";
@@ -60,12 +80,12 @@ $img_ex = "extention TEXT(10)";
 $image_like = "likes INT(10)";
 //$senpai->exec("DROP TABLE images");
 $senpai->exec("CREATE TABLE gallery ($id, $userid, $author,$date,$image)");
+*/
 
-///create like table
+///CREATE LIKE TABLE
 try {
     // hard connect cause dis shit aint workin
-    $dbh = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $senpai = Call_onee_san();
     $sql = "CREATE TABLE `like` (
       `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
       `userid` INT(11) NOT NULL,
@@ -74,9 +94,29 @@ try {
       FOREIGN KEY (userid) REFERENCES users(id),
       FOREIGN KEY (galleryid) REFERENCES gallery(id)
     )";
-    $dbh->exec($sql);
+    $senpai->exec($sql);
     echo "Table like created successfully\n";
-} catch (PDOException $e) {
-    echo "well shit bro!: ".$e->getMessage()."\n";
 }
+catch (PDOException $e) {
+    echo "well shit bro! ~like table: ".$e->getMessage()."\n";
+}
+
+///CREATE COMMENT TABLE
+try {
+	// Connect to DATABASE previously created
+	$senpai = Call_onee_san();
+	$sql = "CREATE TABLE `comments` (
+	  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	  `userid` INT(11) NOT NULL,
+	  `galleryid` INT(11) NOT NULL,
+	  `comment` VARCHAR(255) NOT NULL,
+	  FOREIGN KEY (userid) REFERENCES users(id),
+	  FOREIGN KEY (galleryid) REFERENCES gallery(id)
+	)";
+	$senpai->exec($sql);
+	echo "We can comment on how beautiful and amazing senpai is!\n";
+} catch (PDOException $e) {
+	echo "Senpai doesn't want us to comment ;( (comment table still broken)".$e->getMessage()."\n";
+}
+
 ?>
