a9e5019f787b6af1511e39ac52ace3c9aad030a9
Shane OLIVARI <solivari@c3r6s8.wethinkcode.co.za>
2019-10-28 15:15:37 +0200

    fixed get_specific

--- database/functions.php
+++ database/functions.php
@@ -1,22 +1,42 @@
 <?php
-require("../init.php");
-require("getuser.php");
+require("init.php");
 
+/* ok for get_specific:
+	target = what you want to get.
+	table = the table its in.
+	coloumn = the coloum of the value you have to offer.
+	value = any other reference to what you want, example. 
+	you want the vkey the give the username for who you want the vkey of,
+	and the column will be username
+*/
 function get_specific($target, $table, $column, $value){
-	$senpai = Call_onee_san();
-	$sth = $senpai->prepare("SELECT * FROM $table WHERE $column=$value");
-	$sth->execute();
-	$result = $sth->fetch(PDO::FETCH_ASSOC);
-	return ($result[$target]);
+	try {
+		$senpai = Call_onee_san();
+		$sth = $senpai->prepare("SELECT * FROM $table WHERE $column ='$value'");
+		$sth->execute();
+		$result = $sth->fetch(PDO::FETCH_ASSOC);
+	 print_r($result);
+		$sth->closeCursor();
+		echo ($result[$target])."\n";
+		return ($result[$target]);
+	} catch (PDOException $e) {
+		echo "failed to get_specific".$e->getMessage()."\n";
+	}
 }
 
 function update_specific($target, $new_var, $table, $column, $value){
+try {
 	$senpai = Call_onee_san();
 	$sth = $senpai->prepare("UPDATE $table SET $target='$new_var' WHERE $column=$value");
 	$sth->execute();
+	$sth->closeCursor();
+} catch (PDOException $e) {
+	echo "failed to update specific\n";
+}
 }
 
 function find_specific($var, $column, $table){
+try {
 	$senpai = Call_onee_san();
 	$sth = $senpai->prepare("SELECT $column FROM $table");
 	$sth->execute();
@@ -24,13 +44,18 @@ function find_specific($var, $column, $table){
 		if ($var == $result[$column])
 			return (1);
 	}
+	$sth->closeCursor();
 	return (0);
+} catch (PDOException $e) {
+	echo "failed to find specific\n";
+}
 }
 
 function delete_specific($table, $column, $value){
 	$senpai = Call_onee_san();
 	$sth = $senpai->prepare("DELETE FROM $table WHERE $column=$value");
 	$sth->execute();
+	$sth->closeCursor();
 }
 
 /////// for add user
@@ -50,6 +75,7 @@ function hasher($password){
 }
 
 function add_user($username, $email, $display, $password){
+try {
 	if (find_specific($username,"username","users"))
 		return ("username taken");
 	if ($display)
@@ -60,14 +86,16 @@ function add_user($username, $email, $display, $password){
 	$vkey = random_key("6");
 	$onee_chan = $senpai->prepare("INSERT INTO users $column VALUES ('$username','$email','$display','$hashed_pass','$vkey')");
 	$onee_chan->execute();
+} catch (PDOException $e) {
+	echo "failed to add user: ".$e->getMessage()."\n";
 }
-
-
-
+}
 
 add_user("Shane","shane@gmail.com", "", "shane");
 add_user("PC","PC@gmail.com", "", "PC");
 add_user("marvy","marthen@gmail.com", "", "marvan");
 add_user("Crillin","crillin@gmail.com", "", "DBZ");
+
+get_specific("vkey", "users", "username", "PC");
 
 ?>
