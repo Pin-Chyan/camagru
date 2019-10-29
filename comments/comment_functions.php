22444380b61dfdee49d6bcaa8da6acea7b4af97b
Liam Krieling <lkrielin@c3r6s9.wethinkcode.co.za>
2019-10-29 13:54:20 +0200

    comment functions

--- comments/comment_functions.php
+++ comments/comment_functions.php
@@ -1,6 +1,20 @@
 <?php
 include_once("database/init.php");
 
+function find_comment($userid,$galleryid){
+    try {
+        $senpai = call_onee_san();
+        $id = 1;
+        $exec = $senpai->prepare("SELECT * FROM comments");
+        $exec->execute();
+        while (($res = $exec->fetch(PDO::FETCH_ASSOC))){
+            print_r($res);
+        }
+    }
+    catch (PDOException $err){
+        echo $err."\n";
+    }
+}
 function add_comments($userid,$galleryid,$text){
     try {
         $senpai = Call_onee_san();
@@ -9,9 +23,13 @@ function add_comments($userid,$galleryid,$text){
         echo "comment added\n";
     }
     catch(PDOException $err){
-        echo $err."\n";
+        echo $arr."\n";
     }
 }
-
-add_comments("1","1","this is a comment");
+// add_comments("1","1","comment added");
+// add_comments("1","1","comment added");
+// add_comments("1","1","comment added");
+// add_comments("1","1","comment added");
+// add_comments("1","1","comment added");
+find_comment("1","1");
 ?>
