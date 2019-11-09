332ad7eb72df5a8a12846e4e1c5d6c54fbaafebd
Shane OLIVARI <solivari@c2r4s6.wethinkcode.co.za>
2019-11-09 17:42:43 +0200

    likes

--- api/like.php
+++ api/like.php
@@ -3,22 +3,26 @@
 require_once("../header.php");
 // session_start();
 
+// echo "The time is " . date("h:i:sa") . "<br/>";
+// echo "post test ok ".$_POST['id']."<br/>";
+// print_r($_POST);
+// echo "<br/> get test ok <br/>";
+// print_r($_GET);
+
 $user_id = get_specific("id", "users", "username", $_SESSION['user_id']);
 $id = $_POST['galleryid'];
 
-$res = is_liked("1", "1", 'like');
-
-print_r($res);
+is_liked("1", "1", 'like');
 
 $action = $_REQUEST['operation'];
-// if ($res = is_liked($user_id, $id, 'like')) {
-//     basic_html("test",$res);
-//     print_r($res);
-// }
-// else if ($action === "like"){
-//         add_like($user_id,$id);
-// }
+if (is_liked($user_id, $id, 'like')) {
+    delete_like("like", "userid", $user_id, "galleryid", $id);
+}
+else if ($action === "like"){
+        add_like($user_id,$id);
+}
 $i = $_REQUEST['page'];
+print_r($i);
 
-// header("Location: ../home_html.php?page=$i");
+header("Location: ../home_html.php?page=$i");
 ?>
