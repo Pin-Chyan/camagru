86ff3963ff310a3226faa18248d45cb9d9091dd7
Marthen van der Eems <marvan-d@c3r8s7.wethinkcode.co.za>
2019-11-13 09:18:18 +0200

    Added vieport to some pages

--- user_page.php
+++ user_page.php
@@ -85,7 +85,7 @@ function update_notify($name, $pref) {
 	<head>
 		<title>Profile</title>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
 	<body>
 <!-- 
